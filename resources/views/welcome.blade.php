ac7317c47c89611d030de9db51472b01ad01edcd
Groupe MO <web.groupe.mo@gmail.com>
2023-07-26 15:45:48 +0200

    Final commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Laravel/VueJS3</title>
     @vite('resources/css/app.css')
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
 <body>
 <main class="form-signin">
@@ -14,9 +15,9 @@
             @auth
                 <a href="{{ route('dashboard') }}" class="btn button-dashboard">Dashboard</a>
             @else
-                <a href="{{ route('login') }}" class="button-login btn" style="margin: 15px; padding: 15px">Log in</a>
+                <a href="{{ route('login') }}" class="btn btn-success" style="margin: 15px; ">Log in</a>
                  @if (Route::has('register'))
-                    <a href="{{ route('register') }}" class="button btn btn-primary btn-lg">Register</a>
+                    <a href="{{ route('register') }}" class="btn btn-outline-primary">Register</a>
                 @endif
             @endauth
         </div>
@@ -25,5 +26,6 @@
 <main id="app">
 </main>
 @vite('resources/js/app.js')
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 </html>
