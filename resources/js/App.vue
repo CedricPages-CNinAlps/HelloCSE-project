de88192dabbca9906cf13f2f8fe41a66d8d1ead3
Groupe MO <web.groupe.mo@gmail.com>
2023-07-26 11:52:05 +0200

    Design 1er version

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -3,19 +3,16 @@
         <h1 class="py-8 text-5xl font-bold text-center">
             Profil Browser
         </h1>
-        <Test />
         <Profile />
     </div>
 </template>
 
 <script>
 import Profile from "./components/Profile.vue";
-import axios from "axios";
-import Test from "./components/Test.vue";
 
 export default {
     name: "App",
-    components: {Test, Profile},
+    components: {Profile},
 }
 </script>
 
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,6 +12,11 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link href="http://127.0.0.1:8000/" target="_blank">
+                        {{ __('Le site front') }}
+                    </x-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
