f125f4c3f64c79ceb928176b03f034e545cf2866
Groupe MO <web.groupe.mo@gmail.com>
2023-07-26 15:37:50 +0200

    Design version

--- resources/js/components/Profile.vue
+++ resources/js/components/Profile.vue
@@ -1,36 +1,27 @@
 <template>
-        <div class="container flex flex-wrap">
-            <div class="names w-full sm:w-1/4 p-4">
-                <!-- La liste des noms -->
-                <ul>
-                    <li
-                        v-for="name in starsaxios"
-                        :key="name.id"
-                        @click="selectName(name)"
-                        class="cursor-pointer"
-                    >
-                        {{ name.lastname }} {{ name.firstname }}
-                    </li>
-                </ul>
-            </div>
-            <div class="info w-full sm:w-3/4 p-4">
-                <!-- L'emplacement pour afficher les informations liées au nom sélectionné -->
-                <div v-if="selectedName" class="info-content">
-                    <img
-                        class="float-left"
-                        :src="selectedName.image"
-                        style="width: 150px"
-                    />
-                    <h2 class="py-8 text-3xl font-bold">
-                        {{ selectedName.firstname }} {{ selectedName.lastname }}
-                    </h2>
-                    <p>{{ selectedName.description }}</p>
+    <div class="flex-wrap">
+        <ul role="list" class="p-6 divide-y divide-slate-200">
+            <li class="cursor-pointer flex py-4"
+                v-for="name in starsaxios"
+                :key="name.id"
+                @click="toggleNameContent(name)">
+                <div class="ml-3 overflow-clip sm:w-1/6" >
+                    <p class="text-sm font-medium">{{ name.firstname }} {{ name.lastname }}</p>
                 </div>
-                <div v-else>
-                    <p>Sélectionnez un nom pour afficher les informations</p>
+                <div v-if="selectedName && (name.lastname == selectedName.lastname)"
+                     class="sm:w-3/4 bg-white overflow-hidden md:max-w-2xl">
+                    <div class="md:flex">
+                        <img class="float-left border-4 mr-6 h-36 md:h-full md:w-48 shadow-md" :src="selectedName.image"
+                             :alt="selectedName.firstname + ' ' +selectedName.lastname">
+                        <div class="tracking-wide text-xl text-black font-semibold">
+                            {{ selectedName.firstname }} {{ selectedName.lastname }}
+                        </div>
+                        <p class="mt-2 text-slate-500">{{ selectedName.description }}</p>
+                    </div>
                 </div>
-            </div>
-        </div>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -40,13 +31,17 @@ export default {
     name: "Profile",
     data() {
         return {
-            starsaxios: {},
+            starsaxios: [],
             selectedName: null,
         }
     },
     methods: {
-        selectName(name) {
-            this.selectedName = name;
+        toggleNameContent(name) {
+            if (this.selectedName && this.selectedName.lastname === name.lastname) {
+                this.selectedName = null;
+            } else {
+                this.selectedName = name;
+            }
         },
         getResult() {
             axios.get('/data/starsList')
@@ -59,7 +54,7 @@ export default {
         },
     },
     created() {
-        this.getResult()
+        this.getResult();
     },
 }
 </script>
